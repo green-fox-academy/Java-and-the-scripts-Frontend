9dbb39bdc2d2530901f3ef4c94d1f51ae0f00de2
Kitti <kittitoth8@gmail.com>
2019-05-23 13:32:48 +0200

    feat(myqueue): creating myqueue component

---
+++ Queuing/src/app/components/my-queue/my-queue.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-queue',
+  templateUrl: './my-queue.component.html',
+  styleUrls: ['./my-queue.component.css']
+})
+export class MyQueueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
