9c2ffb75fab204ce3bf1126d00ae49310ee8165b
Orsi Nagy <orsolya.n91@gmail.com>
2019-05-24 09:21:27 +0200

    Trying to fix doctor selector bug

--- Queuing/src/app/components/doctor-selector/doctor-selector.component.ts
+++ Queuing/src/app/components/doctor-selector/doctor-selector.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { doctor } from 'src/app/models/doctor';
 import { DoctorsService } from 'src/app/services/doctors/doctors.service';
+import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
 
 @Component({
   selector: 'app-doctor-selector',
@@ -13,13 +14,20 @@ export class DoctorSelectorComponent implements OnInit {
   defaultValue = 'Choose your doctor';
   doctors: doctor[] = [];
   @Output() selectedDoc = new EventEmitter<string>();
+  username: string;
 
   constructor(
     private formBuilder: FormBuilder,
-    private doctorService: DoctorsService
+    private doctorService: DoctorsService,
+    private authenticationService: AuthenticationService
   ) {}
 
   ngOnInit() {
+    this.authenticationService.authState.subscribe(user => {
+      if (user) {
+        this.username = user.username;
+      }
+    });
     this.form = this.formBuilder.group({
       doctor: this.selectedDoc ? this.selectedDoc : this.defaultValue
     });
