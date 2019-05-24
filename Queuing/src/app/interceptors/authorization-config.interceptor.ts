8e357457321381f83c3007cb9afce220d583f19b
Orsi Nagy <orsolya.n91@gmail.com>
2019-05-24 08:47:07 +0200

    Adding token checking

--- Queuing/src/app/interceptors/authorization-config.interceptor.ts
+++ Queuing/src/app/interceptors/authorization-config.interceptor.ts
@@ -4,22 +4,40 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Injectable, Input } from '@angular/core';
+import { Injectable, Input, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AuthenticationService } from '../services/authentication/authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthorizationConfigInterceptor implements HttpInterceptor {
+export class AuthorizationConfigInterceptor implements HttpInterceptor, OnInit {
+  user;
+
+  constructor(private authService: AuthenticationService){
+  }
+  
+ngOnInit(){
+this.authService.authState.subscribe((user)=> {
+  if (user){
+    this.user = user;
+  }
+})
+}
+
+  authToken = localStorage.getItem('authToken');
+
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    request = request.clone({
-      setHeaders: {
-        Authorization: `Bearer ${localStorage.getItem('authToken')}`
-      }
-    });
+    if (this.user) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${localStorage.getItem('authToken')}`
+        }
+      });
+    }
     return next.handle(request);
   }
 }
